759e8d216ea695bcad36550e147a0721ee803b6a
lpacheco09 <lpachecoc09@gmail.com>
2023-06-15 19:22:45 -0600

    Add project files.

---
+++ ML/Movie.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ML
+{
+    public class Movie
+    {
+        public int id { get; set; }
+        public string original_title { get; set; }
+        public string overview { get; set; }
+        public string release_date { get; set; }
+        public string backdrop_path { get; set; }
+        public string vote_average { get; set; }
+        public string original_language { get; set; }
+        public string poster_path { get; set; }
+        public List<object> Movies { get; set; }
+    }
+}
---
+++ PL/Controllers/MovieController.cs
@@ -0,0 +1,49 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PL.Controllers
+{
+    public class MovieController : Controller
+    {
+        // GET: Movie
+        public ActionResult Popular()
+        {
+            ML.Movie movie = new ML.Movie();
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://api.themoviedb.org/3/");
+                var responseTask = client.GetAsync("movie/popular?api_key=c9617782375bf747c35d7aafecf16f5b&language=es-MX&page=1");
+                responseTask.Wait();
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsStringAsync();
+                    dynamic resultJSON = JObject.Parse(readTask.Result.ToString());
+                    readTask.Wait();
+                    movie.Movies = new List<object>();
+
+                    foreach (var resultItem in resultJSON.results)
+                    {
+                        ML.Movie movieItem = new ML.Movie();
+                        movieItem.id = resultItem.id;
+                        movieItem.original_title = resultItem.original_title;
+                        movieItem.overview = resultItem.overview;
+                        movieItem.release_date = resultItem.release_date;
+                        movieItem.backdrop_path = resultItem.backdrop_path;
+                        movieItem.vote_average = resultItem.vote_average;
+                        movieItem.original_language = resultItem.original_language;
+                        movieItem.poster_path = resultItem.poster_path;
+                        movie.Movies.Add(movieItem);
+                    }
+                }
+            }
+            return View(movie);
+        }
+    }
+}
